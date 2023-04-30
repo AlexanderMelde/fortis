0dce9e95c9f8923454e5977ad0bc524200517fef
Alexander Melde <2115644+AlexanderMelde@users.noreply.github.com>
2023-04-30 14:16:39 +0200

    fix: user profile xp bar test

--- src/app/pages/user/user.component.spec.ts
+++ src/app/pages/user/user.component.spec.ts
@@ -9,6 +9,7 @@ import {MatExpansionModule} from "@angular/material/expansion";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {MatSelectModule} from "@angular/material/select";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {InfobannerComponent} from "../../ui-components/infobanner/infobanner.component";
@@ -27,7 +28,7 @@ describe('UserComponent', () => {
       declarations: [UserComponent, MatAccordionResponsiveComponent, InfobannerComponent],
       imports: [MatCardModule, MatDividerModule, MatExpansionModule, MatFormFieldModule, MatSelectModule,
         MatCheckboxModule, MatDatepickerModule, MatLuxonDateModule, BrowserAnimationsModule, MatIconModule,
-        FormsModule, ReactiveFormsModule, MatInputModule]
+        FormsModule, ReactiveFormsModule, MatInputModule, MatProgressBarModule]
     })
       .compileComponents();
 
