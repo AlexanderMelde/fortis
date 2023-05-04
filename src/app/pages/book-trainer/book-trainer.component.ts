4635039d4c22891ca4ea040a57e87657d4ee5326
Alexander Melde <2115644+AlexanderMelde@users.noreply.github.com>
2023-05-04 23:41:18 +0200

    feat: add book trainer page
    Signed-off-by: Alexander Melde <2115644+AlexanderMelde@users.noreply.github.com>

--- src/app/pages/book-trainer/book-trainer.component.ts
+++ src/app/pages/book-trainer/book-trainer.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {BreakpointObserver} from "@angular/cdk/layout";
+import {Component} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {StepperOrientation} from "@angular/material/stepper";
+import {DateTime} from "luxon";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-book-trainer',
@@ -6,5 +12,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-trainer.component.css']
 })
 export class BookTrainerComponent {
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  stepperOrientation: Observable<StepperOrientation>;
 
+  protected readonly DateTime = DateTime;
+
+  constructor(private _formBuilder: FormBuilder, breakpointObserver: BreakpointObserver) {
+    this.stepperOrientation = breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({matches}) => (matches ? 'horizontal' : 'vertical')));
+  }
 }
